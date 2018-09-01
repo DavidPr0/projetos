fa6ffa067a7ca3b3dcf75b8c6eeafd11c94ff6be
David dos Santos <david25@globomail.com>
2018-08-31 22:58:32 -0300

    Primeiro APP com JavaScript Calculadora

---
+++ projetos/JavaScript/assets/js/meu-js.js
@@ -0,0 +1,7 @@
+// alert('teste');
+
+// Ele pega o elemento do documento pelo ID e atribui um novo valor
+// document.getElementById('nome').value = 'Oi'
+
+var texto = 'Curso de JavaScriot'
+document.write('<h1>'+ texto + '</h1>')
