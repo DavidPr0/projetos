dc2f7a9e5239e2e6306ea0acf8fcf23a5b5b255e
David dos Santos <david25@globomail.com>
2018-09-13 23:34:35 -0300

    Iniciando App Lista de despesas

---
+++ projetos/ECMAScript/app03-orçamento-pessoal/assets/js/app.js
@@ -0,0 +1,29 @@
+/**********************************
+***********Fictory class***********
+***********************************/
+class Despesa{
+	constructor(ano, mes, dia, tipo, descricao, valor){
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+
+	}
+}
+/**********************************
+***Função que cadasta as despesas**
+***********************************/
+function cadastrarDespesa(){
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
+	
+	let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+
+	console.log(despesa)
+}
