7c3fce2d146fc5acc493a81bf7c2454dee5008ce
David dos Santos <david25@globomail.com>
2018-10-01 22:28:15 -0300

    v1

---
+++ projetos/PHP/variaveis_constantes.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Curso de PHP</title>
+</head>
+<body>
+	<?php 
+	//Variáveis constantes
+	
+	define('BD_URL', 'endereco_bd_dev');
+	define('BD_USUARIO', 'usuario_dev');
+	define('BD_SENHA', 'senha_dev');
+
+	echo BD_URL.'<br/>';
+	echo BD_USUARIO. '<br/>';
+	echo BD_SENHA. '<br/>';
+
+
+	?>
+</body>
+</html>
